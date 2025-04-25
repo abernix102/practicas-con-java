41211a845a62fba90fd4d54fd99cebfe66d2a572
Nahuel <nahuelo841@gmail.com>
2025-04-25 15:42:02 -0300

    ejercicios

---
+++ HolaMundo/src/holamundo/EmpresaDeVuelosMatrices.java
@@ -0,0 +1,44 @@
+package holamundo;
+
+public class EmpresaDeVuelosMatrices {
+    public static void main(String[] args) {
+        /*
+Una compañía de vuelos cuenta con 6 destinos a los que realiza 3 vuelos diariamente,
+uno por la mañana, otro al mediodía y otro por la noche.
+Para administrar estos datos, utiliza una matriz, donde cada fila es un destino
+y en cada columna se guarda la cantidad de asientos disponibles.
+Por ejemplo:
+
+                 Mañana   Mediodía   Noche
+Rio de Janeiro   120        15        30
+Cancún            65         1        32
+Madrid             4        52         7
+Roma              46        23        16
+Milán             64         2        11
+Iguazú            61        12        91
+
+
+i.
+Se necesita un programa que permita la carga de la matriz
+con la cantidad de asientos para cada vuelo.
+
+ii.
+Al mismo tiempo, el programa debe permitir a un usuario ingresar:
+- el número de destino al que quiere dirigirse,
+- el número de vuelo (dependiendo si quiere viajar a la mañana, al mediodía o a la noche),
+- y la cantidad de pasajes que necesita.
+
+iii.
+A partir de la solicitud del usuario, el programa debe:
+- controlar si hay la cantidad suficiente de asientos para la cantidad de pasajes que se requiere.
+- En caso de que así sea, se debe mostrar un cartel en pantalla que diga
+  "su reserva fue realizada con éxito"
+  y se debe descontar del total de asientos los solicitados por el usuario.
+- En caso de no haber más asientos disponibles, se debe informar otro cartel que diga
+  "disculpe, no se pudo completar su operación dado que no hay asientos disponibles".
+
+La compañía de vuelos manifiesta que NO CONOCEN cuántas ventas/reservas se hacen por día.
+Por lo cual, para finalizar las ventas, se ingresa la palabra "finish".
+*/
+    }
+}
--- HolaMundo/src/holamundo/ejerciciosDeTodoCode.java
+++ HolaMundo/src/holamundo/ejerciciosDeTodoCode.java
@@ -1,10 +1,12 @@
 package holamundo;
 
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Scanner;
 
 public class ejerciciosDeTodoCode {
     public static void main(String[] args) {
-        int[] loadNumber = {1, 2, 3, 4, 5, 3, 6, 7, 8, 9, 10, 5, 11, 12, 1};
+       /*
+       int[] loadNumber = {1, 2, 3, 4, 5, 3, 6, 7, 8, 9, 10, 5, 11, 12, 1};
         ArrayList<Integer> numerosRepetidos = new ArrayList<>();
         for(int i = 0; i < loadNumber.length; i++){
             for(int j = i + 1 ;  j < loadNumber.length; j++){
@@ -16,5 +18,31 @@ public class ejerciciosDeTodoCode {
             }
         }
         System.out.println(numerosRepetidos);
+        */
+        double[][] notas = new double[4][4];
+        Scanner scanner = new Scanner(System.in);
+        for (int f = 0; f < notas.length; f++) {
+            double suma = 0;
+            if (f == 0) {
+                System.out.println("ingrese las 3 primeras notas del primer alumno");
+            } else {
+                System.out.println("ingrese las 3 notas del alumno " + (f + 1) + ".");
+            }
+            for (int c = 0; c < notas[f].length - 1; c++) {
+                boolean notaValida = false;
+                while (!notaValida) {
+                    double valorDeNota = scanner.nextDouble();
+                    if (valorDeNota >= 0 && valorDeNota <= 10) {
+                        notas[f][c] = valorDeNota;
+                        suma += valorDeNota;
+                        notaValida = true;
+                    } else {
+                        System.out.println("❌ La nota debe estar entre 0 y 10. Ingresá de nuevo:");
+                    }
+                }
+                notas[f][3] = Math.round((suma / 3) * 100.0) / 100.0;
+            }
+        }
+        System.out.println(Arrays.deepToString(notas));
     }
 }
